cbb09a12306928e598e148d7e48268dc6f772336
kinglemuel <pro@kinglemuel.com>
2016-08-12 04:27:03 -0400

    add test for brands, update all test

--- spec/factoriesSpec.js
+++ spec/factoriesSpec.js
@@ -16,28 +16,37 @@ describe('Factories', function () {
             .expect(200)
             .end(function(err, res) {
                 if (err) return done.fail(res);
+                console.log(res.body);
                 expect(res.body.length).toBeGreaterThan(0);
                 done(res);
             });
     });
     it('gets a single factory', function (done) {
         request(app)
-            .get('/factories/0a75d3f4-c8ff-47bb-84c3-a874007d1b4f')
+            .get('/factories/1a537038-b78a-402e-a461-82cdda48aaf3')
             .expect(200)
             .end(function(err, res) {
                 if (err) return done.fail(res);
-                expect(res.body).not.toBeNull();
+                expect(res.body.company_type).toEqual('factory');
                 done(res);
             });
     });
     it('creates a new factory', function (done) {
         request(app)
             .post('/factories')
-            .send({ name: 'Test Factory'})
+            .send({ name: 'Test Factory',
+                    email: "test_factory@gmail.com",
+                    phone_number: "(718)555-5555",
+                    city: "New York",
+                    state:"NY"})
             .end(function (err, res) {
                 if (err) return done.fail(res);
                 expect(res.body.name).toEqual('Test Factory');
-
+                expect(res.body.email).toEqual('test_factory@gmail.com');
+                expect(res.body.phone_number).toEqual('(718)555-5555');
+                expect(res.body.city).toEqual('New York');
+                expect(res.body.state).toEqual('NY');
+                expect(res.body.company_type).toEqual('factory');
                 done(res);
             });
     })
