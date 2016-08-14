7df9acc19b37c80a31e59ad54128831753d9cb59
kinglemuel <pro@kinglemuel.com>
2016-08-14 03:30:35 -0400

    add test for homepage h1

--- spec/homePageSpec.js
+++ spec/homePageSpec.js
@@ -1,5 +1,6 @@
-casper.test.begin("Homepage has links to factories and brands", 4, function suite(test){
+casper.test.begin("Homepage has links to factories and brands", 5, function suite(test){
   casper.start('http://localhost:3000/', function(){
+    test.assertSelectorHasText('h1', 'Makers Row');
     test.assertExists('a#factories', '"factories" link is found');
     test.assertSelectorHasText('a#factories', 'Factories');
     test.assertExists('a#brands', '"brands" link is found');
